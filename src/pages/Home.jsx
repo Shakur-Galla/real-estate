93071a0782d5b965d16cc6092019e3bb1ac82bc2
MetaphorX <gall.abdulshakur@gmail.com>
2025-06-08 12:39:06 -0700

    featured section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import StatisticsGrid from '../components/StatisticsGrid'
+import FeaturedProperties from "../components/FeaturedProperties";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <StatisticsGrid/>
+      <FeaturedProperties />
     </>
   );
 };
